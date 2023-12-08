5d7ae254e3ffdd8e78ff317b110dedc028c098c8
YumNumm <onory0917@gmail.com>
2023-12-08 16:58:58 +0900

    fix:

--- android/app/src/main/kotlin/net/yumnumm/flutter_compose_view/ui/CounterApp.kt
+++ android/app/src/main/kotlin/net/yumnumm/flutter_compose_view/ui/CounterApp.kt
@@ -4,15 +4,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -20,6 +31,30 @@ import io.flutter.plugin.common.MethodChannel
 fun CounterApp(
     methodChannel: MethodChannel
 ) {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+
+    LaunchedEffect(Unit) {
+        println("Counter LaunchedEffect")
+        fun onMethodCall(methodCall: MethodCall, result: MethodChannel.Result) {
+            when (methodCall.method) {
+                "increment" -> {
+                    counter.value++
+                    result.success(counter.value)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+        methodChannel.setMethodCallHandler(::onMethodCall)
+    }
+
+    fun onCounterUpdated(): Unit {
+        methodChannel.invokeMethod("update", counter.value)
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -32,6 +67,16 @@ fun CounterApp(
                 }
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    counter.value++
+                    onCounterUpdated()
+                },
+            ) {
+                Icon(Icons.Filled.Add, "Small floating action button.")
+            }
+        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -40,9 +85,17 @@ fun CounterApp(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Counter(
-                methodChannel = methodChannel
-            )
+                Text(
+                    text = "You have pushed the button this many times:"
+                )
+                Text(
+                    text = "${counter.value}",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
         }
     }
 }
+
